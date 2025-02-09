a5e72af21a40e9f53ef9aed9c2968c1414f7b7c0
kudakwashechikovo17 <k.chikovo@alustudent.com>
2025-02-09 10:19:25 -0800

    humidity in descending order

--- shell_script.sh
+++ shell_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sort -t, -k3 -n -r raw_data/satelite_temperature_data.csv | uniq | head -10 > analyzed_data/highest_temp.csv
+grep Angola raw_data/satelite_temperature_data.csv | awk -F, '!seen[$4]++' | sort -t, -k4 -n -r > analyzed_data/humidity_data_Angola.csv 
+
 
