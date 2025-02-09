ff1561b574283deeb40cadff38981a8e050a33b3
armstrong_codes <a.hirwa2@alustudent.com>
2025-02-09 12:13:25 -0800

    Removed unnecessary files and added some changes

--- shell_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sort -t, -k3 -n -r raw_data/satelite_temperature_data.csv | uniq | head -10 > analyzed_data/highest_temp.csv
-grep Angola raw_data/satelite_temperature_data.csv | awk -F, '!seen[$4]++' | sort -t, -k4 -n -r > analyzed_data/humidity_data_Angola.csv 
-
-
